098a28607dfc11f0b128e948afb5e8ea0b7dda91
riza_ramadan <riza.ramadan@gmail.com>
2021-01-13 11:42:58 +0700

    Get profile implemented.
    `/profiles/{username}` is implemented but with caveat. Following feature still not implemented, so it will return false all the time

--- src/Contracts/Users/User.cs
+++ src/Contracts/Users/User.cs
@@ -21,4 +21,12 @@
         public string Image { get; set; }
         public string Password { get; set; }
     }
+
+    public class Profile
+    {
+        public string Username { get; set; }
+        public string Bio { get; set; }
+        public string Image { get; set; }
+        public bool Following { get; set; }
+    }
 }
